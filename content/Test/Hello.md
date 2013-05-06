b862a4ecc2fac93aade37a83219020c5373eb7b2
Sadhanandh Vishwanath <sathanandh@gmail.com>
2013-05-07 00:34:09 +0530

    First commit

---
+++ content/Test/Hello.md
@@ -0,0 +1,23 @@
+Title: Hello World
+Slug: xbuntu-xfce-blog
+Date: 06-05-2013
+Tags: ubuntu,xfce
+
+Hi this is my first blog post.
+Hope that all are glad with this project. If anyone isnt then you should visit the drafts sections.
+
+	:::python 
+	import os
+	def hello():
+		print "Hi"
+		print os.path.abspath(".")
+
+This is cpp
+
+	:::cpp
+	#include<iostream>
+	using namespace std;
+	int main()
+	{
+		cout<<"Hi";
+	}
---
+++ pelicanconf.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Sadhanandh V'
+SITENAME = u'Code Trance'
+SITEURL = ''
+
+TIMEZONE = 'Asia/Kolkata'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+#FEED_ALL_ATOM = None
+#CATEGORY_FEED_ATOM = None
+#FEED_ALL_ATOM = 'feeds/all.atom.xml'
+#CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+
+
+
+TRANSLATION_FEED_ATOM = None
+TWITTER_USERNAME = "sadhanandh_v"
+DISQUS_SITENAME = "codetrance"
+GITHUB_URL = "http://github.com/Sadhanandh/"
+#MENUITEMS = (("Hello","#"),("World","#"),)
+#THEME = "notmyidea"
+#THEME = "chunk"
+#CSS_FILE = "wide.css"
+
+# Blogroll
+#LINKS =  (('Github', 'http://github.com/sadhanandh'),
+#          ('Here', '#'),)
+
+# Social widget
+SOCIAL =  (('Github', 'http://github.com/sadhanandh'),		
+			('Twitter', 'https://www.twitter.com/sadhanandh_v'),)
+
+#DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
